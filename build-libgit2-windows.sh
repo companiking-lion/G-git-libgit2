1bb1610b5cb48b8743fd046b1a57d0865c4b4b25
John Medland <john@serenial.io>
2019-12-29 17:52:38 +0000

    Expanding building functions to include openssl and libssh2

--- build-libgit2-windows.sh
+++ build-libgit2-windows.sh
@@ -9,7 +9,7 @@ CMAKE_GENERATOR_ARG="Visual Studio 14 2015"
 #CMAKE_GENERATOR_ARG="Visual Studio 16 2019" -A "x64"
 
 #CMAKE_BUILD_TYPE=Debug
-CMAKE_BUILD_TYPE=Release
+CMAKE_BUILD_TYPE="Release"
 
 ## TODO - Add command line arguments to specify build
 
@@ -21,8 +21,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 VENDOR_PATH=vendor
 BUILD_DIR=$DIR/$VENDOR_PATH/build
 
-#zlib paths
-ZLIB_LIB_OUTPUT_DIR=$DIR/build/libs
+#lib output paths
+LIB_OUTPUT_DIR=$DIR/build/libs
 
 function createDir {
     # create directory but silence stderr by redirecting it
@@ -30,24 +30,58 @@ function createDir {
 }
 
 function buildZLIB {
-    ZLIB_DIR="zlib"
+    LIB_DIR="zlib"
 
     echo "Building ZLIB with $CMAKE_GENERATOR_ARG"
     
     cd $BUILD_DIR
 
     # clean zlib directory
-    rm -rf $ZLIB_DIR
+    rm -rf $LIB_DIR
 
     # create zlib build-dir
-    mkdir $ZLIB_DIR
-    cd $ZLIB_DIR
+    mkdir $LIB_DIR
+    cd $LIB_DIR
 
     # call cmake
     # use eval so arguments don't get truncated
-    eval "cmake ../../zlib -G \"$CMAKE_GENERATOR_ARG\" -D LIBRARY_OUTPUT_PATH=\"$ZLIB_LIB_OUTPUT_DIR\" -D INSTALL_INC_DIR=\"$ZLIB_LIB_OUTPUT_DIR\" -D INSTALL_LIB_DIR=\"$ZLIB_LIB_OUTPUT_DIR\""
+    eval "cmake ../../zlib -G \"$CMAKE_GENERATOR_ARG\" -D LIBRARY_OUTPUT_PATH=\"$LIB_OUTPUT_DIR\" -D INSTALL_INC_DIR=\"$LIB_OUTPUT_DIR\" -D INSTALL_LIB_DIR=\"$LIB_OUTPUT_DIR\""
 
     cmake --build . --config $CMAKE_BUILD_TYPE
+}
+
+
+function buildOPENSSL{
+    # this is going to be difficult
+    # https://wiki.openssl.org/index.php/Compilation_and_Installation
+    # https://stackoverflow.com/questions/40007633/how-to-compile-openssl-on-windows
+    # nasm need to be installed and on the system path
+    # https://www.nasm.us/
+
+
+
+}
+
+
+function buildLIBSSH2 {
+    LIB_DIR="libssh2"
+
+    echo "Building libssh2 with $CMAKE_GENERATOR_ARG"
+    
+    cd $BUILD_DIR
+
+    # clean zlib directory
+    rm -rf $LIB_DIR
+
+    # create zlib build-dir
+    mkdir $LIB_DIR
+    cd $LIB_DIR
+
+    # call cmake
+    # use eval so arguments don't get truncated
+    #eval "cmake ../../zlib -G \"$CMAKE_GENERATOR_ARG\" -D LIBRARY_OUTPUT_PATH=\"$ZLIB_LIB_OUTPUT_DIR\" -D INSTALL_INC_DIR=\"$ZLIB_LIB_OUTPUT_DIR\" -D INSTALL_LIB_DIR=\"$ZLIB_LIB_OUTPUT_DIR\""
+
+    #cmake --build . --config $CMAKE_BUILD_TYPE
 }
 
 echo "synchronizing git submodules"
@@ -56,6 +90,8 @@ echo "synchronizing git submodules"
 # create build dir
 createDir $BUILD_DIR
 
-buildZLIB
+#buildZLIB
+
+#buildLIBSSH2
 
 
